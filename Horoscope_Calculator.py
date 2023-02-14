4ab1e081250afdefff47a46234a5ed5994b443f9
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-14 13:37:31 +0000

    initial commit

---
+++ Horoscope_Calculator.py
@@ -0,0 +1,19 @@
+#birthdate prompt
+
+starsigns = ["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" ]
+
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+gradebook = [["Capricorn", 1], ["Aquarius", 2], ["Pisces", 3], ["Aries", 4], ["Taurus", 5], ["Gemini", 6], ["Cancer", 7], ["Leo", 8], ["Virgo", 9], ["Libra", 10], ["Scorpio", 11], ["Sagittarius", 12]]
+
+birthdate = birthdate = input("What is your birth date (dd/mm/yyyy)? ")
+    if birthdate = */01/*
+    then
+    print("You are" + print(starsigns[0]))
+
+    elif birthdate = */02/*
+    then
+
+
+
+python Horoscope_Calculator.py
