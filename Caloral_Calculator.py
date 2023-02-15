2580d96bf23794c8207504130d766a208cc9e149
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-15 16:47:02 +0000

    Try hard

--- Caloral_Calculator.py
+++ Caloral_Calculator.py
@@ -1,1 +1,18 @@
+#optimal weight for men and women, equal to their height for men, -5 kilos of their height for women
 
+gender = input("What is your sex? (m/f) ")
+
+if input != "m"
+ print("Invalid Parameter") and exit = 0
+
+height = input("What is your height? (in cm) ")
+
+weight = input("What is your weight? (in kg) ")
+
+optimal_weight = 0
+
+if gender == "m":
+    then print("Your optimal weight is (height - 100)")
+
+if gender == "f" or gender == "female":
+    then print("Your optimal weight is (height - 105)")
