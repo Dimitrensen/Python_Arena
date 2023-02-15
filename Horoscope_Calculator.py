c175558f2b6c8d5030bbea5ef2641246a82a3a61
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-15 11:58:22 +0000

    Excercise solved, the 8  year old way

--- Horoscope_Calculator.py
+++ Horoscope_Calculator.py
@@ -2,18 +2,40 @@
 
 starsigns = ["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" ]
 
-months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+birthmonth = input("Which month where you born? ")
 
-gradebook = [["Capricorn", 1], ["Aquarius", 2], ["Pisces", 3], ["Aries", 4], ["Taurus", 5], ["Gemini", 6], ["Cancer", 7], ["Leo", 8], ["Virgo", 9], ["Libra", 10], ["Scorpio", 11], ["Sagittarius", 12]]
+if birthmonth == "January":
+    print("You are " + starsigns[0])
 
-birthdate = birthdate = input("What is your birth date (dd/mm/yyyy)? ")
-    if birthdate = */01/*
-    then
-    print("You are" + print(starsigns[0]))
+if birthmonth == "February":
+    print("You are " + starsigns[1])
 
-    elif birthdate = */02/*
-    then
+if birthmonth == "March":
+    print("You are " + starsigns[2])
 
+if birthmonth == "April":
+    print("You are " + starsigns[3])
 
+if birthmonth == "May":
+    print("You are " + starsigns[4])
 
-python Horoscope_Calculator.py
+if birthmonth == "June":
+    print("You are " + starsigns[5])
+
+if birthmonth == "July":
+    print("You are " + starsigns[6])
+
+if birthmonth == "August":
+    print("You are " + starsigns[7])
+
+if birthmonth == "February":
+    print("You are " + starsigns[8])
+
+if birthmonth == "February":
+    print("You are " + starsigns[9])
+
+if birthmonth == "February":
+    print("You are " + starsigns[10])
+
+if birthmonth == "December":
+    print("You are " + starsigns[11])
