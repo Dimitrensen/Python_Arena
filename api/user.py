fc0bc0721623b3a4811339e4b0cb3d94f13c6791
Alexandros Papadopoulos <al.pap@live.com>
2023-02-22 22:07:01 +0100

    Add files via upload

---
+++ api/user.py
@@ -0,0 +1,10 @@
+import classes
+
+def create_user:
+  mock_user = classes.User("asjhdad","adasdasd",classes.role.Admin)
+
+# import <package> from  <library>
+# import <library>
+
+#(manually fix user details, preferences, etc. so when we do a print everything to be created)
+# if you print the user class remember to use user.__dict__
