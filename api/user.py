f51aac1d8bf14765293d7f6e348b02803fa573d6
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-23 12:49:33 +0000

    user_pref added, however pref class is missing.

--- api/user.py
+++ api/user.py
@@ -2,9 +2,13 @@ import classes
 
 def create_user:
   mock_user = classes.User("asjhdad","mucha@cz",classes.role.Admin)
+
+#(manually fix user details, preferences, etc. so when we do a print everything to be created)
+# if you print the user class remember to use user.__dict__
   
   mock_user_details = classes.userDetails(
     name="Mia"
+    age=25
     body_type=classes.bodyType.lean,
     hair_color=classes.hairColor.Blond,
     personality_type=classes.personalityType.INTJ,
@@ -15,14 +19,20 @@ def create_user:
     description="I am have mean cooking skills and I am very patient."
   )
 
-  mock_user_preferences = ?
-
-   # create user preferences instance
-    user_pref = classes.userPref(
-
+  user_pref = classes.userPref(
+    preferred_age_range=(23, 35),
+    preferred_body_type=[classes.bodyType.chubby, classes.bodyType.athletic]
+    preferred_hair_color=[classes.hairColor.Blond, classes.hairColor.Red],
+    preferred_personality_type=classes.personalityType.ENFJ,
+    preferred_hobbies=[classes.hobbies.Running, classes.hobbies.Hiking],
+    preferred_zodiac=[classes.zodiac.Aries, classes.zodiac.Sagittarius],
+    preferred_gender=classes.gender.Male,
+    preferred_location=classes.location(longitude=47.1, latitude=33.8)
+    )
 # import <package> from  <library>
 # import <library>
 
-#(manually fix user details, preferences, etc. so when we do a print everything to be created)
-# if you print the user class remember to use user.__dict__
+return mock_user
 
+user = create_user()
+print(user.__dict__)
