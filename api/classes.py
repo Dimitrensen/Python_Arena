5ba26e7a99174a49a79946d1310629b0678cf2ac
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-25 17:34:41 +0000

    Username generator fixed

--- api/classes.py
+++ api/classes.py
@@ -1,108 +1,120 @@
 
 class location:
-  def __init__(self,longitude,latitude):
-    self.latitude=latitude
-    self.longitude=longitude
+	def __init__(self,longitude,latitude):
+		self.latitude=latitude
+		self.longitude=longitude
 
 
-class username:
-  def username_generator(name, zodiac, body_type):
-    username = name[:2] + zodiac[:2] + body_type[:2]
-    return username
 
-  #new_account = account_generator(first_name, last_name)
+
+	#new_account = account_generator(first_name, last_name)
 
 class personalityType:
-  ESTJ=0
-  ENTJ=1
-  ESFJ=2
-  ENFJ=3
-  ISTJ=4
-  ISFJ=5
-  INTJ=6
-  INFJ=7
-  ESTP=8
-  ESFP=9
-  ENTP=10
-  ENFP=11
-  ISTP=12
-  ISFP=13
-  INTP=14
-  INFP=15
+	ESTJ=0
+	ENTJ=1
+	ESFJ=2
+	ENFJ=3
+	ISTJ=4
+	ISFJ=5
+	INTJ=6
+	INFJ=7
+	ESTP=8
+	ESFP=9
+	ENTP=10
+	ENFP=11
+	ISTP=12
+	ISFP=13
+	INTP=14
+	INFP=15
 
 
 class zodiac:
-  Aries=1
-  Taurus=2
-  Gemini=3
-  Cancer=4
-  Leo=5
-  Virgo=6
-  Libra=7
-  Scorpio=8
-  Sagittarius=9
-  Capricorn=10
-  Aquarius=11
-  Pisces=12
+	Aries=1
+	Taurus=2
+	Gemini=3
+	Cancer=4
+	Leo=5
+	Virgo=6
+	Libra=7
+	Scorpio=8
+	Sagittarius=9
+	Capricorn=10
+	Aquarius=11
+	Pisces=12
 
 
 class bodyType:
-  athletic=0
-  chubby=1
-  lean=2
-  petite=3
-  masculine=4
+	athletic=0
+	chubby=1
+	lean=2
+	petite=3
+	masculine=4
 
 
 class hobbies:
-  Football=0
-  Yoga=1
-  Games=2
-  Cooking=3
-  Running=4
-  Dancing=5
-  Hiking=6
+	Football=0
+	Yoga=1
+	Games=2
+	Cooking=3
+	Running=4
+	Dancing=5
+	Hiking=6
 
 
 class hairColor:
-  Brunette=0
-  Blond=1
-  Black=2
-  Red=3
-  Blue=4
+	Brunette=0
+	Blond=1
+	Black=2
+	Red=3
+	Blue=4
 
 
 class gender:
-  Female=0
-  Male=1
-  Other=2
+	Female=0
+	Male=1
+	Other=2
 
 
 class role:
-  User=0
-  Admin=1
-  Superuser=2
+	User=0
+	Admin=1
+	Superuser=2
 
 
 class userDetails(object):
-  def __init__(self,name,age,body_type,hair_color,personality_type,hobbies,zodiac,gender,location,description):
-    self.name=name
-    self.body_type=body_type
-    self.hair_color=hair_color
-    self.personality_type=personality_type
-    self.hobbies=hobbies
-    self.zodiac=zodiac
-    self.gender=gender
-    self.location=location
-    self.description=description
-    self.age=age
+	def __init__(self,name,age,body_type,hair_color,personality_type,hobbies,zodiac,gender,location,description):
+		self.name=name
+		self.body_type=body_type
+		self.hair_color=hair_color
+		self.personality_type=personality_type
+		self.hobbies=hobbies
+		self.zodiac=zodiac
+		self.gender=gender
+		self.location=location
+		self.description=description
+		self.age=age
 
 
 class User(object):
-  def __init__(self,role,email,password,username):
-    self.role=role
-    self.username=username.username_generator
-    self.email=email
-    self.password=password
-    self.userDetails
-    self.userPref
+	def __init__(self,role,email,password):
+		self.role=role
+		self.email=email
+		self.password=password
+		self.userDetails
+		self.userPref
+		self.username_generator()
+
+	def username_generator(self):
+		self.username = self.email[:3] + self.role[:2]
+
+
+class userPreferences(object):
+	def __init__(self,age,body_type,hair_color,personality_type,hobbies,zodiac,gender,location):
+		self.age=age
+		self.body_type=body_type
+		self.hair_color=hair_color
+		self.personality_type=personality_type
+		self.hobbies=hobbies
+		self.zodiac=zodiac
+		self.gender=gender
+		self.location=location
--- api/user.py
+++ api/user.py
@@ -1,38 +1,38 @@
 import classes
 
-def create_user:
-  mock_user = classes.User("asjhdad","mucha@cz",classes.role.Admin)
+def create_user():
+  mock_user = classes.User(classes.role.Admin,"mucha@cz",155678, )
 
 #(manually fix user details, preferences, etc. so when we do a print everything to be created)
 # if you print the user class remember to use user.__dict__
   
   mock_user_details = classes.userDetails(
-    name="Mia"
-    age=25
-    body_type=classes.bodyType.lean,
-    hair_color=classes.hairColor.Blond,
-    personality_type=classes.personalityType.INTJ,
-    hobbies=[classes.hobbies.Running, classes.hobbies.Hiking],
-    zodiac=classes.zodiac.Leo,
-    gender=classes.gender.Female,
-    location=classes.location(longitude=45.5, latitude=34.2),
-    description="I am have mean cooking skills and I am very patient."
+    "Mia",
+    25,
+    classes.bodyType.lean,
+    classes.hairColor.Blond,
+    classes.personalityType.INTJ,
+    [classes.hobbies.Running, classes.hobbies.Hiking],
+    classes.zodiac.Leo,
+    classes.gender.Female,
+    classes.location(longitude=45.5, latitude=34.2),
+    "I am have mean cooking skills and I am very patient."
   )
 
-  user_pref = classes.userPref(
-    preferred_age_range=(23, 35),
-    preferred_body_type=[classes.bodyType.chubby, classes.bodyType.athletic]
-    preferred_hair_color=[classes.hairColor.Blond, classes.hairColor.Red],
-    preferred_personality_type=classes.personalityType.ENFJ,
-    preferred_hobbies=[classes.hobbies.Running, classes.hobbies.Hiking],
-    preferred_zodiac=[classes.zodiac.Aries, classes.zodiac.Sagittarius],
-    preferred_gender=classes.gender.Male,
-    preferred_location=classes.location(longitude=47.1, latitude=33.8)
+  user_preferences = classes.userPreferences(
+    (23, 35),
+    [classes.bodyType.chubby, classes.bodyType.athletic]
+    [classes.hairColor.Blond, classes.hairColor.Red],
+    classes.personalityType.ENFJ,
+    [classes.hobbies.Running, classes.hobbies.Hiking],
+    [classes.zodiac.Aries, classes.zodiac.Sagittarius],
+    classes.gender.Male,
+    classes.location(longitude=47.1, latitude=33.8)
     )
 # import <package> from  <library>
 # import <library>
 
-return mock_user
+  return mock_user
 
 user = create_user()
 print(user.__dict__)
